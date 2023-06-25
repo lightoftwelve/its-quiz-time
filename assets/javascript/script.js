99f5428aad1a5ec3bb5aa308c3c28ade74784712
lightoftwelve <info@lightoftwelve.com>
2023-06-25 01:47:17 -0400

    added several sections of to the HTML code, basic layout for the javascript code and added large updates to css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,39 @@
-// to ensure header runs on each HTML page
+// Ensures the header is loaded on each HTML page
 fetch('header.html')
-    // then is a type of method that works with js promises to handle asynchronous operations. Then executes the next steps once the promise is resolved (ie when it successfully completes task)
+    // The 'then' method handles asynchronous operations and executes the next steps once the promise is resolved (i.e., when it successfully completes the task)
     // response.text extracts content from requested page in plain text to bring header to the pages its called
     .then(response => response.text())
     .then(content => {
         const headerElement = document.getElementById('header');
         headerElement.innerHTML = content;
     })
-    // catch handles errors that happen while fetching header while console.error prints the error into the console for debugging
+    // Handles any errors that occur while fetching the header and logs them to the console for debugging
     .catch(error => {
         console.error('Error loading header:', error);
     });
+
+// NOTE: Because the header is asynchronous, it won't block the execution of the rest of the code
+
+var rulesContainer = document.getElementById("rulesContainer");
+var countdownContainer = document.getElementById("countdownContainer");
+var quizContainer = document.getElementById("quizContainer");
+var quizNextButton = document.getElementById("next-btn");
+var quizStartButton = document.getElementById("start-btn");
+
+quizNextButton.addEventListener(`click`, function () {
+    rulesContainer.style.display = "none";
+    countdownContainer.style.display = "block";
+});
+
+    // General Plan:
+    // Declare variables to keep track of score
+    // Implement a way to select 15 random questions from a pool of 40
+    // Have questions load one at a time
+    // Play sound whether the answer is right or wrong
+    // Figure out how to add countdown music for the last 10 seconds, also add a mute button
+    // Implement a scoring point system
+    // Add a timer that awards points based on how fast the user answers and deducts 15 seconds if the answer is wrong
+    // Possibly create two timers? One for the overall quiz, and one for each question. give harder questions more time?
+    // Continue with the remaining questions
+    // Implement initial score keeping using local storage
+    // Add functionality to reset the game
